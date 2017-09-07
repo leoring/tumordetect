b94e0d79709de28d0d932e0c474a762e6f7dee9a
leoring <lening@smmail.cn>
2017-09-07 16:29:27 +0800

    Add files via upload

---
+++ RandomDrawSamples.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+			
+""" 
+    Randomly Draw Training & Testing Samples
+    2017.8.30
+"""
+
+from __future__ import division, print_function, absolute_import
+
+import os
+import tensorflow as tf
+import random
+import sys, shutil
+
+#define paramaters
+tf.app.flags.DEFINE_string('pre_postive','../data/presamples/postive/',"directory which save original postive samples")
+tf.app.flags.DEFINE_string('pre_negtive','../data/presamples/negtive/',"directory which save original negtive samples")
+tf.app.flags.DEFINE_string('post_postive','../data/postsamples/postive/',"directory which save selected postive samples")
+tf.app.flags.DEFINE_string('post_negtive','../data/postsamples/negtive/',"directory which save selected negtive samples")
+tf.app.flags.DEFINE_string('save_path','../data/postsamples/',"directory which save results")
+tf.app.flags.DEFINE_boolean('debug_mode', True, "Debug mode")
+FLAGS = tf.app.flags.FLAGS
+
+#get file number in selected directory
+def filenumber(FilePath):
+    count = 0
+    for root, dirs, files in os.walk(FilePath):
+        fileLength = len(files)
+        if fileLength != 0:
+            count = count + fileLength
+    return count
+
+def IsSubString(SubStrList,Str):
+    
+    flag=True
+    for substr in SubStrList:
+        if not(substr in Str):
+            flag=False
+    return flag
+
+#get the file name in selected directory
+def CopyNegtiveSamples(FlagStr=[], Ratio = 1.0):
+    
+    FileList=[]
+    
+    RND = random.randint(0, int(Ratio))
+
+    count = 0     
+    FileNames=os.listdir(FLAGS.pre_negtive)
+
+    all_the_text = ''
+    if (len(FileNames)>0):
+        for fn in FileNames:
+            if (len(FlagStr)>0):
+                if (IsSubString(FlagStr,fn)):
+                    if(Ratio > 0.99 and Ratio < 1.5):
+                        shutil.copy(FLAGS.pre_negtive + fn, FLAGS.post_negtive + fn)
+                        all_the_text = all_the_text + FLAGS.post_negtive + fn + ' 0' + '\n'
+
+                    elif(count % int(Ratio) == RND):
+                        shutil.copy(FLAGS.pre_negtive + fn, FLAGS.post_negtive + fn)
+                        all_the_text = all_the_text + FLAGS.post_negtive + fn + ' 0' + '\n'
+
+                    FileList.append(fn)
+        
+                else:
+                    if(Ratio > 0.99 and Ratio < 1.5):
+                        shutil.copy(FLAGS.pre_negtive + fn, FLAGS.post_negtive + fn)
+                        all_the_text = all_the_text + FLAGS.post_negtive + fn + ' 0' + '\n'
+
+                    elif(count % int(Ratio) == RND):
+                        shutil.copy(FLAGS.pre_negtive + fn,FLAGS.post_negtive + fn)
+                        all_the_text = all_the_text + FLAGS.post_negtive + fn + ' 0' + '\n'
+
+                    FileList.append(fn)
+                    
+                count = count + 1
+    
+    if (len(FileList)>0):
+        FileList.sort()
+    
+    return all_the_text
+
+def CopyPostiveSamples(FlagStr=[]):
+    
+    FileList=[]
+    
+    FileNames=os.listdir(FLAGS.pre_postive)
+
+    all_the_text = ''
+    if (len(FileNames)>0):
+        for fn in FileNames:
+            if (len(FlagStr)>0):
+                if (IsSubString(FlagStr,fn)):
+                    
+                    shutil.copy(FLAGS.pre_postive + fn, FLAGS.post_postive + fn)
+                    all_the_text = all_the_text + FLAGS.post_postive + fn + ' 1' + '\n'
+
+                    FileList.append(fn)
+        
+                else:
+                    shutil.copy(FLAGS.pre_postive + fn, FLAGS.post_postive + fn)
+                    all_the_text = all_the_text + FLAGS.post_postive + fn + ' 1' + '\n'
+
+                    FileList.append(fn)
+     
+    if (len(FileList)>0):
+        FileList.sort()
+    
+    return all_the_text
+
+PostiveNum = filenumber(FLAGS.pre_postive)
+NegtiveNum = filenumber(FLAGS.pre_negtive)
+print('NegtiveNum:',NegtiveNum)
+
+#copy all postive samples to post sample sets
+all_the_text = CopyPostiveSamples('jpg')
+
+#draw negtive samples according to ratio between negtive & postive samples
+ratio = 1.0
+if (PostiveNum > 0):
+    ratio = 1.0 * NegtiveNum / PostiveNum
+
+all_the_text = all_the_text + CopyNegtiveSamples('jpg',ratio)
+
+#write filename & IOU type;
+file_object = open(FLAGS.save_path + 'filelist.txt', 'w+')
+file_object.write(all_the_text)
+file_object.close( )
+    
---
+++ ResidualNet.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+""" 
+This source code is created by Le Ning (lening@sjtu.edu.cn) on August 30, 2017.
+This is training code based on residual net.
+"""
+
+from __future__ import division, print_function, absolute_import
+import tensorflow as tf													
+import tflearn
+from tflearn.data_utils import shuffle, to_categorical
+from tflearn.layers.core import input_data, dropout, flatten
+from tflearn.layers.conv import conv_2d, max_pool_2d, avg_pool_2d
+from tflearn.layers.estimator import regression
+import pickle
+import numpy as np 
+from PIL import Image
+import os.path
+
+#define paramaters
+tf.app.flags.DEFINE_string('file_path','../data/postsamples/',"directory which include original cancer or no-cancer pictures")
+tf.app.flags.DEFINE_integer('sample_size', 64, "sample size")
+tf.app.flags.DEFINE_integer('nepoch', 100, "epoch number")
+tf.app.flags.DEFINE_integer('batchsize', 64, "batch size number")
+FLAGS = tf.app.flags.FLAGS
+
+def load_image(img_path):
+    img = Image.open(img_path)
+    return img
+
+def resize_image(in_image, new_width, new_height, out_image=None,
+                 resize_mode=Image.ANTIALIAS):
+    img = in_image.resize((new_width, new_height), resize_mode)
+    if out_image:
+        img.save(out_image)
+    return img
+
+def pil_to_nparray(pil_image):
+    pil_image.load()
+    return np.asarray(pil_image, dtype="float32")
+
+def load_data(datafile, num_clss, save=False, save_path='dataset.pkl'):
+    train_list = open(datafile,'r')
+    labels = []
+    images = []
+    for line in train_list:
+        tmp = line.strip().split(' ')
+        fpath = tmp[0]
+        print(fpath)
+        img = load_image(fpath)
+        img = resize_image(img,FLAGS.sample_size,FLAGS.sample_size)
+        np_img = pil_to_nparray(img)
+        images.append(np_img)
+
+        index = int(tmp[1])
+        label = np.zeros(num_clss)
+        label[index] = 1
+        labels.append(label)
+    if save:
+        pickle.dump((images, labels), open(save_path, 'wb'))
+    return images, labels
+
+# Data loading
+# Load path/class_id image file:
+dataset_file = FLAGS.file_path + 'filelist.txt'
+
+# Build the preloader array, resize images to sample size
+num_classes = 2
+X, Y = load_data(dataset_file, num_classes)
+
+# preprocessing
+img_prep = tflearn.ImagePreprocessing()
+
+# Real-time data augmentation
+img_aug = tflearn.ImageAugmentation()
+img_aug.add_random_flip_leftright()
+
+# Building 'residual network'
+n = 5
+net = tflearn.input_data(shape=[None, FLAGS.sample_size, FLAGS.sample_size, 3],
+                         data_preprocessing=img_prep,
+                         data_augmentation=img_aug)
+
+net = tflearn.conv_2d(net, 16, 3, regularizer='L2', weight_decay=0.0001)
+net = tflearn.residual_block(net, n, 16)
+net = tflearn.residual_block(net, 1, 32, downsample=True)
+net = tflearn.residual_block(net, n-1, 32)
+net = tflearn.residual_block(net, 1, 64, downsample=True)
+net = tflearn.residual_block(net, n-1, 64)
+net = tflearn.batch_normalization(net)
+net = tflearn.activation(net, 'relu')
+net = tflearn.global_avg_pool(net)
+
+# Regression
+net = tflearn.fully_connected(net, num_classes, activation='softmax')
+mom = tflearn.Momentum(0.1, lr_decay=0.1, decay_step=30000, staircase=True)
+net = tflearn.regression(net, optimizer=mom, loss='categorical_crossentropy')
+
+# Training
+model = tflearn.DNN(net, checkpoint_path='model_resnet_tumor',
+                    max_checkpoints=10, tensorboard_verbose=0,
+                    clip_gradients=0.)
+
+model.fit(X, Y, n_epoch=FLAGS.nepoch,validation_set=0.1,
+          snapshot_epoch=False, snapshot_step=200,
+          show_metric=True, batch_size=FLAGS.batchsize, shuffle=True,
+          run_id='resnet_tumor')
+
+model.save('residual_net')
+
